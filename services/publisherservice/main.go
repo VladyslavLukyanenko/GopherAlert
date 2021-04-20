771cedd9bcf9d493797637411805866ba8d7e383
Vladyslav Lukyanenko <me@vladyslav.xyz>
2021-04-20 14:49:52 +0200

    Oops
    Signed-off-by: Vladyslav Lukyanenko <me@vladyslav.xyz>

--- services/publisherservice/main.go
+++ services/publisherservice/main.go
@@ -17,37 +17,7 @@ func main() {
 	log.SetLevel(log.DebugLevel)
 	configs.ReadConfig()
 	initAmqp()
-	webhook := core.Webhook{
-		Notification:              core.Notification{
-			ChannelName: "togglebit",
-			Title: "Rust | Vim | Making a pixel editor | Command input, then some networking malarkey",
-			Avatar: "https://static-cdn.jtvnw.net/jtv_user_pictures/0bb9c502-ab5d-4440-9c9d-14e5260ebf86-profile_image-70x70.png",
-			Game: "Science & Technology",
-			Viewers: 163,
-			URI: "https://www.twitch.tv/togglebit",
-		},
-		URI:                    "https://hooks.slack.com/services/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",
-		DeliveryPlatform:       core.Slack,
-		NotificationType:       core.Live,
-		MonitoringPlatformType: core.Twitch,
-	}
-	payload, err := json.Marshal(webhook)
-	for true {
-		err = ch.Publish(
-			"publisher-service-exchange", // exchange
-			"publisher-service",      // routing key
-			false,              // mandatory
-			false,              // immediate
-			amqp.Publishing{
-				DeliveryMode: amqp.Transient,
-				ContentType:  "application/json",
-				Body:         payload,
-			})
-	}
-	if err != nil {
-
-	}
-	//go handler(messages)
+	go handler(messages)
 	select {}
 }
 
@@ -82,41 +52,41 @@ func initAmqp() {
 
 	}
 	log.Debugf("Declared exchange %s", "publisher-service-exchange")
-	//q, err := ch.QueueDeclare(
-	//	"publisher-service-queue",
-	//	true,
-	//	false,
-	//	false,
-	//	false,
-	//	nil,
-	//)
-	//if err != nil {
-	//	log.Error(err.Error())
-	//	log.Panic("Error while declaring queue")
-	//	return
-	//}
-	//log.Debugf("Declared queue %s", q.Name)
-	//err = ch.QueueBind(
-	//	q.Name,
-	//	"publisher-service",
-	//	"publisher-service-exchange",
-	//	false,
-	//	nil,
-	//)
-	//if err != nil {
-	//	log.Panic("Error while binding queue")
-	//	return
-	//}
-	//log.Debugf("Binded to queue %s", q.Name)
-	//messages, err = ch.Consume(
-	//	q.Name,
-	//	"pubisher-service",
-	//	true,
-	//	false,
-	//	false,
-	//	false,
-	//	nil,
-	//)
+	q, err := ch.QueueDeclare(
+		"publisher-service-queue",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Error(err.Error())
+		log.Panic("Error while declaring queue")
+		return
+	}
+	log.Debugf("Declared queue %s", q.Name)
+	err = ch.QueueBind(
+		q.Name,
+		"publisher-service",
+		"publisher-service-exchange",
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Panic("Error while binding queue")
+		return
+	}
+	log.Debugf("Binded to queue %s", q.Name)
+	messages, err = ch.Consume(
+		q.Name,
+		"pubisher-service",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
 
 	log.Debug("Connected to RabbitMQ")
 }
